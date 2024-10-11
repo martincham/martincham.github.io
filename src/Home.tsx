694124e20ca7cfd1123fb53199999f789ea60a0d
martincham <martinchamberlin12@gmail.com>
2024-10-11 14:16:53 +0200

    add hubris vid

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,8 +3,12 @@ import React from "react";
 const Home: React.FC = () => {
   return (
     <div className="container mx-auto p-6">
-      <section className="container mx-auto py-12">
-        <a>I'm a software developer and an artist.</a>
+      <section className="container mx-auto py-2">
+        <img
+          src="/HQPictures/Hubris/hubrisBlur.webp"
+          alt="Hubris"
+          className="w-full h-auto max-w-screen max-h-[800px] object-contain"
+        />
       </section>
     </div>
   );
