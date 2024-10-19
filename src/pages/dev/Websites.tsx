0f81a5e8cbfa08af82d6b748f56095f9de1216c2
martincham <martinchamberlin12@gmail.com>
2024-10-19 12:52:06 -0400

    fix client routing

--- src/pages/dev/Websites.tsx
+++ src/pages/dev/Websites.tsx
@@ -15,14 +15,19 @@ const Websites: React.FC = () => {
     <div className="container mx-auto p-6">
       <section className="container mx-auto py-2">
         <p>
-          I built my first simple HTML/CSS portfolio website in 2016. Since
-          then, I expanded my web-dev toolkit with JavaScipt and TypeScript in
-          2019. Most recently I built this website using a modern tech-stack.
+          I built my first HTML/CSS website in 2016. I expanded my web-dev
+          toolkit with JavaScipt and TypeScript in 2019, and then learned React.
         </p>
         <Separator className="my-2" />
         <div className="mx-auto flex  flex-col items-center justify-center gap-6 p-4 sm:p-6 lg:p-8">
           <div className="grid w-full gap-6 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
             {/* Website Cards */}
+            <GitHubCard
+              repoName="martinchamberlin.com"
+              description="Built with React,TypeScript/JavaScript, Tailwind CSS, and shadcn"
+              repoLink="https://github.com/martincham/martincham.github.io"
+            />
+            {/* unlisted.studio Card */}
             <Card className="lg:max-w-md" x-chunk="charts-01-chunk-0">
               <CardHeader>
                 <CardTitle>
@@ -34,23 +39,16 @@ const Websites: React.FC = () => {
                   </a>
                 </CardTitle>
               </CardHeader>
-              <CardContent></CardContent>
-
               <CardFooter className="flex-col items-start gap-2 text-sm border-t">
                 <div className="flex gap-2 font-medium leading-none pt-2">
                   Gallery show - Fall 2023.
                 </div>
                 <div className="leading-none text-muted-foreground">
-                  The chosen aesthetic was "Craigslist". Simple HTML/CSS website
-                  with photos and descriptions of each artist.
+                  A simple HTML/CSS website with photos and descriptions of each
+                  artist. The chosen aesthetic was "Craigslist".
                 </div>
               </CardFooter>
             </Card>
-            <GitHubCard
-              repoName="martinchamberlin.com"
-              description="Built with React,TypeScript/JavaScript, Tailwind CSS, and shadcn"
-              repoLink="https://github.com/martincham/martincham.github.io"
-            />
           </div>
         </div>
       </section>
