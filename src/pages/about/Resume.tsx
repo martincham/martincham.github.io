fd57bad03b90f0a368ee2c43d8f3e96cef6974be
martincham <martinchamberlin12@gmail.com>
2024-12-01 20:13:39 -0500

    fix pdf on mobile

--- src/pages/about/Resume.tsx
+++ src/pages/about/Resume.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 
 const Resume: React.FC = () => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  const pdfUrl = "/files/MartinChamberlinResume.pdf";
   return (
     <div className="container mx-auto p-6 ">
       <section className="container mx-auto py-2">
-        <iframe
-          src="/files/MartinChamberlinResume.pdf"
-          className="w-full h-screen"
-          style={{ border: "none" }}
-          title="Embedded Resume PDF"
-        ></iframe>
+        {isMobile ? (
+          <a
+            href={pdfUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 underline"
+          >
+            Download Resume
+          </a>
+        ) : (
+          <iframe
+            src={pdfUrl}
+            className="w-full h-screen"
+            style={{ border: "none" }}
+            title="Embedded Resume PDF"
+          ></iframe>
+        )}
       </section>
     </div>
   );
